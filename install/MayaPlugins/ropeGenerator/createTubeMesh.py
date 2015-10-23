8816321273835fcb3d5da0f7cb0fe9b329d55fdc
Ignacio Urruty <iniaki84@gmail.com>
2015-10-23 01:19:08 -0300

    ya estoy mas cerquita, aunque sea dibuja algo :)

--- install/MayaPlugins/ropeGenerator/createTubeMesh.py
+++ install/MayaPlugins/ropeGenerator/createTubeMesh.py
@@ -3,14 +3,15 @@ import maya.OpenMaya as om
 def getMatrixFromParamCurve( curveFn, param ):
 	"""docstring for getMatrixFromParamCurve"""
 	pDivPos = om.MPoint()
-	curveFn.getPointAtParam( 0, pDivPos, om.MSpace.kWorld );
-	vNormal = om.MVector( curveFn.normal( 0, om.MSpace.kObject ) );
-	vTangent = om.MVector( curveFn.tangent( 0, om.MSpace.kObject ) );
+	curveFn.getPointAtParam( param, pDivPos, om.MSpace.kWorld );
+	print pDivPos.x, pDivPos.y, pDivPos.z
+	vNormal = om.MVector( curveFn.normal( param, om.MSpace.kObject ).normal() );
+	vTangent = om.MVector( curveFn.tangent( param, om.MSpace.kObject ).normal() );
 	vExtra = vNormal ^ vTangent
 	dTrans =[
-			vExtra.x, vExtra.y, vExtra.z, 0.0,
-			vTangent.x, vTangent.y, vTangent.z, 0.0,
 			vNormal.x, vNormal.y, vNormal.z, 0.0,
+			vTangent.x, vTangent.y, vTangent.z, 0.0,
+			vExtra.x, vExtra.y, vExtra.z, 0.0,
 			pDivPos.x,pDivPos.y,pDivPos.z, 1.0]
 	mTrans = om.MMatrix();
 	om.MScriptUtil.createMatrixFromList(dTrans,mTrans)
@@ -20,13 +21,13 @@ def getMatrixFromParamCurve( curveFn, param ):
 def createCirclePoints( pointsCount, bMatrix, points ):
 	"""docstring for createCirclePoints"""
 	#points = om.MFloatPointArray()
-	baseVector = om.MVector( 0,0,1 )
-	baseVector2 = om.MVector( 0,0,1 ) * bMatrix
+	baseVector = om.MPoint( 1,0,0 )
+	baseVector2 = om.MPoint( 1,0,0 ) * bMatrix
 	points.append( om.MFloatPoint( baseVector2.x, baseVector2.y, baseVector2.z, 1.0 ) )
 	rotVector = om.MVector( 0, 1, 0 )
 	for d in range( 1, pointsCount ):
-		vVector = baseVector.rotateBy( om.MVector.kYaxis, om.MAngle(( 360.0 / pointsCount * d ), om.MAngle.kDegrees).asRadians() )
-		vVector = vVector * bMatrix
+		vVector = om.MVector( baseVector ).rotateBy( om.MVector.kYaxis, om.MAngle(( 360.0 / pointsCount * d ), om.MAngle.kDegrees).asRadians() )
+		vVector = om.MPoint( vVector ) * bMatrix
 		points.append( om.MFloatPoint( vVector.x, vVector.y, vVector.z, 1.0 ) )
 	return points
 
@@ -40,11 +41,11 @@ def createTube( obj = 'curveShape1' ):
 	curveFn = om.MFnNurbsCurve( dagPath )
 	meshFS = om.MFnMesh()
 	#division throu curve
-	divisions = 5
+	divisions = 15
 	#points in ring
 	pointsCount = 5
-	numVerteces = divisions * pointsCount
-	numFaces = pointsCount + 2
+	numVerteces = ( divisions + 1 ) * pointsCount
+	numFaces = ( pointsCount *  divisions  ) + 2
 
 	points = om.MFloatPointArray()
 	faceConnects = om.MIntArray()
@@ -57,31 +58,24 @@ def createTube( obj = 'curveShape1' ):
 			faceCounts.append( pointsCount )
 			for i in range( pointsCount ):
 				faceConnects.append( i )
-		elif d == divisions:
-			param = float(d) / float( divisions )
-			faceCounts.append( pointsCount )
-			for i in range( pointsCount ): #ERROR
-				faceConnects.append( ( pointsCount * divisions ) + i )
 		else:
 			param = float(d) / float( divisions )
 			for i in range( pointsCount ):
 				faceCounts.append( 4 )
-			for f in range( pointsCount ):
+			for f in range( pointsCount + 1 ):
 				faceConnects.append( ( f + ( d * pointsCount ) ) - pointsCount )
 				faceConnects.append( f + 1  + ( d * pointsCount ) - pointsCount )
 				faceConnects.append( f + 1  + ( d * pointsCount ) )
 				faceConnects.append( ( f + ( d * pointsCount ) ) )
+			if d == divisions:
+				faceCounts.append( pointsCount )
+				for i in range( pointsCount ):
+					faceConnects.append( ( pointsCount * ( divisions - 2) ) + i )
 		mTrans = getMatrixFromParamCurve( curveFn, param )
 		createCirclePoints( pointsCount, mTrans, points )
-		faceConnects.append( d )
-
+		#faceConnects.append( d )
+	print numVerteces, numFaces, points.length(), faceCounts.length(), faceConnects.length()
 	newMesh = meshFS.create(numVerteces, numFaces, points, faceCounts, faceConnects )
-	#fIndex = om.MIntArray()
-	#fIndex.append( 0 )
-	#meshFS.extrudeFaces( fIndex, 1, om.MFloatVector( 0, 1, 0 ), True )
-	#meshFS.extrudeFaces( fIndex, 1, om.MFloatVector( 0, 1, 0 ), True )
-	#meshFS.updateSurface()
-	#nodeName = meshFS.name()
 
 
 createTube()
